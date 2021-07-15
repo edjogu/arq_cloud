b7bc953056fff44016254882591a31e80b3ea8d9
edjogu <59851244+edjogu@users.noreply.github.com>
2021-07-15 18:04:36 -0500

    commit update semana 4

---
+++ _curso/_semana4/ejercicio-funcionInterna.py
@@ -0,0 +1,12 @@
+print(max("Hola Mundo"))
+print(min("Hola Mundo"))
+
+print(int(345.7899))
+
+mum_mayor = max(4,12,43.3,19,100)
+print(f"El número mayor es: {mum_mayor}")
+
+print(float(32))
+print(float("3,1415"))
+# print(str(32))
+# print(str(45.987))
---
+++ _curso/_semana4/operaciones-math.py
@@ -0,0 +1,27 @@
+import math
+
+# print(math.sin(45))
+# print(math.sqrt(4))
+# print(math.log10(20))
+
+# def calcular_seno(numero):
+#     # Seno
+#     return math.sin(numero)
+
+# def calcular_sqrt(numero):
+#     # Raiz cuadrada
+#     return math.sqrt(numero)
+
+# def calcular_log(numero):
+#     # Log en base 10
+#     return math.log10(numero)
+
+# print(calcular_seno(45))
+# print(calcular_sqrt(25))
+# print(calcular_log(100))
+# 
+b = 11001
+dec = print(int(str(b), 2))
+
+print(bin(25)[2:])
+
